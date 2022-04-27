1e494645420784806dd46ad7ebbc2cf9863da80a
toor <mr.ano@mail.com>
2022-04-27 09:06:41 +0700

    add stripprefix

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM python:3.8
 
 WORKDIR /code
-COPY ./requirements.txt /code/requirements.txt
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+COPY ./requirements.txt .
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
-COPY ./app /code/app 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
-EXPOSE 8000
+COPY ./app ./app
--- app/main.py
+++ app/main.py
@@ -1,14 +1,37 @@
-from typing import Optional
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-app = FastAPI()
+from app.db import get_session, init_db
+from app.models import Song, SongCreate
+
+from typing import List
+from datetime import datetime
+
+app = FastAPI(root_path='/api/v1')
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/ping")
+async def pong():
+    return {"pong": datetime.now()}
+
+
+@app.get("/songs", response_model=List[Song])
+async def get_songs(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Song))
+    songs = result.scalars().all()
+    return [Song(name=song.name, artist=song.artist, id=song.id) for song in songs]
+
+
+@app.post("/songs")
+async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
+    song = Song(name=song.name, artist=song.artist)
+    session.add(song)
+    await session.commit()
+    await session.refresh(song)
+    return song
