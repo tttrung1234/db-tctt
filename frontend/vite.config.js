b6b3c4dc452e431c2657eb59f5b4aa3bbf45a282
toor <mr.ano@mail.com>
2022-05-22 14:58:25 +0700

    config traefik, fontend:80, backend:8000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,8 +11,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+
   server: {
     host: '0.0.0.0',
-    port: 8000
+    port: 5000
   }
 })
